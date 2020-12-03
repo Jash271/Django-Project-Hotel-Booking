7e707a0c2b8f108e3104f80381ccc44ce80b878f
brahim024 <ifninos168@gmail.com>
2020-12-03 20:55:34 +0100

    add cange test

--- mod3/urls.py
+++ mod3/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings 
 from django.conf.urls.static import static
-#test
+#tests change in upstream
 urlpatterns = [
     path('',include('hotels.urls')),
     path('admin/', admin.site.urls),
