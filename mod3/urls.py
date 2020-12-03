d9504ba5bdc7809fe4530856af97ceaf0345ffa5
brahim024 <ifninos168@gmail.com>
2020-12-03 20:38:18 +0100

    stued change

--- mod3/urls.py
+++ mod3/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-     path('',include('hotels.urls')),
+    path('',include('hotels.urls')),
     path('admin/', admin.site.urls),
 ]
 
